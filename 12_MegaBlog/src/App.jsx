eea2d370567af4fce5068ce996bd4fa16652ae03
rajukumar7 <rajukumar7bth@gmail.com>
2024-02-18 04:07:30 +0530

    config  file

--- 12_MegaBlog/src/App.jsx
+++ 12_MegaBlog/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-
+import React from "react";
 import "./App.css";
 
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
-      <h1 className="bg-green text-center">a blog with app write</h1>
+      <h1 className="bg-green-500 text-center">a blog with app write</h1>
     </>
   );
 }
